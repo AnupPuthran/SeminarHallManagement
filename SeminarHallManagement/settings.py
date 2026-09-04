461a6a2898fb45e8aaf5c376fff5e364a9023aa7
G.Anup <anupputhran04@gmail.com>
2026-09-04 14:24:40 +0530

    Update settings for production environment
    Changed DEBUG setting to False and updated ALLOWED_HOSTS.

--- SeminarHallManagement/settings.py
+++ SeminarHallManagement/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-09vl0+frxncrg$etm#l9(k+f1o60k9npo6+-t=_00b6nh)99hn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
